698fde86990cdbe14d7253dc64959fda7fbdf667
Jiawei Zhang <jiaweizhang95@gmail.com>
2016-04-19 15:44:15 -0400

    Displaying hardcoded characters in screen is possible now

--- v1/prod.asm
+++ v1/prod.asm
@@ -3,12 +3,40 @@
 main:
 
 addi $r10, $r0, 1 # $r10 contains which char 0-indexed (0-3071) mem location
-# as well as the pixel location
+
+jal writeCharToVGA
+
+addi $r10, $r0, 66 # $r10 contains which char 0-indexed (0-3071) mem location
+
+jal writeCharToVGA
+
+addi $r10, $r0, 67 # $r10 contains which char 0-indexed (0-3071) mem location
+
+jal writeCharToVGA
+
+addi $r10, $r0, 63 # $r10 contains which char 0-indexed (0-3071) mem location
+
+jal writeCharToVGA
+
+addi $r10, $r0, 64 # $r10 contains which char 0-indexed (0-3071) mem location
+
+jal writeCharToVGA
+
+addi $r10, $r0, 3071 # $r10 contains which char 0-indexed (0-3071) mem location
+
+jal writeCharToVGA
+
+addi $r10, $r0, 3070 # $r10 contains which char 0-indexed (0-3071) mem location
+
+jal writeCharToVGA
+
+j finish
+
+writeCharToVGA:
 
 # r10 contains char 0-indexed mem location (0-3071)
 
-
-lw $23, 3072($r10) r23 contains desired pixel location
+lw $23, 3072($r10) # r23 contains desired pixel location
 lw $24, 0($r10) # r24 contains char bitmap location (10000-12900)
 
 # r24 should contain bitmapped location
@@ -30,12 +58,15 @@ j doCol
 
 doRow:
 addi $r6, $r6, -1 
-beq $r6, $r0, finish # while still rows left to do
+beq $r6, $r0, finishedChar # while still rows left to do
 
 addi $r23, $r23, 630 # drop down one row to the beginning of char
 addi $r7, $r0, 10 # number of cols to iterate
 j doCol
 
+finishedChar:
+
+jr $r31
 
 finish:
 j finish
