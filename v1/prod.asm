65bc6993bfe980238e636df67d4b5ad7badb94d8
jiaweizhang <jiaweizhang95@gmail.com>
2016-04-19 15:15:24 -0400

    Fixed comments

--- v1/prod.asm
+++ v1/prod.asm
@@ -8,7 +8,7 @@ addi $r10, $r0, 1 # $r10 contains which char 0-indexed (0-3071) mem location
 # r10 contains char 0-indexed mem location (0-3071)
 
 
-lw $23, 3027($r10) r23 contains desired pixel location
+lw $23, 3072($r10) r23 contains desired pixel location
 lw $24, 0($r10) # r24 contains char bitmap location (10000-12900)
 
 # r24 should contain bitmapped location
@@ -22,7 +22,7 @@ beq $r7, $r0, doRow # while still cols left to do
 lw $r4, 0($r24) # $r4 now contains pixel to write
 vga $r4, 0($r23) # store pixel to vga
 
-addi $r23, $r23, 1 # add one to address
+addi $r23, $r23, 1 # add one to pixel address
 addi $r24, $r24, 1   # add one to bitmap location
 
 addi $r7, $r7, -1 # subtract one
